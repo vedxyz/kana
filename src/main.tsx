061aff5295675715d6addcf48ac7d13fda3d5e3c
Ved <dev@vedat.xyz>
2023-04-02 02:03:39 +0300

    Add Mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
 import App from "./App";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: "dark" }}>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 );
