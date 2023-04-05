46198dc73a6fa1cee030afb49dd60718919bb3ac
Ved <dev@vedat.xyz>
2023-04-05 05:17:21 +0300

    Add information to brute force mode menu

--- src/components/BruteForceMenu.tsx
+++ src/components/BruteForceMenu.tsx
@@ -1,4 +1,4 @@
-import { Button, ButtonProps, Container, Group, Text, Title } from "@mantine/core";
+import { Button, ButtonProps, Container, Group, List, Text, Title } from "@mantine/core";
 import React, { useState } from "react";
 import { KanaNames } from "../utilities/kana";
 import BruteForcePractice from "./BruteForcePractice";
@@ -20,6 +20,26 @@ function BruteForceMenu() {
   return (
     <Container px={0}>
       <Text>
+        In this mode you will go through learning stages that introduce you to a new set of kana each time. The answer
+        for newly encountered kana will be shown automatically, pay extra attention to the shape of these kana. After
+        each learning stage, there is a review stage of every kana you have learnt so far. Each stage has a certain
+        requirement to clear, as you will see.
+      </Text>
+      <Title order={4} mt="sm">
+        Tips
+      </Title>
+      <List pr={16}>
+        <List.Item>
+          Do not feel pressured to move onto the next stage immediately when prompted, gauge yourself.
+        </List.Item>
+        <List.Item>You can continue from any stage you want within the options menu.</List.Item>
+        <List.Item>You should expect to need a total of ~1600 correct answers to clear all stages.</List.Item>
+        <List.Item>
+          Learning kana here will not necessarily make you able to <Text span>produce</Text> them. The only guarantee is
+          that you will be able to recognize kana on sight, which is enough (or at least a good start) for most people.
+        </List.Item>
+      </List>
+      <Text mt="sm">
         You should learn{" "}
         <Text span italic>
           hiragana
